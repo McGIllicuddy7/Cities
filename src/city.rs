4671fb586ab1aed37c4a26dd9254e5b4d199101a
Bridget Hartin Pasco <isaacpasco@icloud.com>
2024-06-19 18:35:47 -0700

    added city mod

---
+++ src/city.rs
@@ -0,0 +1,16 @@
+use crate::context::Context;
+use crate::road;
+pub struct City{
+    roads:Vec<road::Road>
+}
+impl City{
+    pub fn new(radius:f64, context:&Context)->Self{
+        let roads = road::generate_roads(radius, context);
+        return Self{roads:roads};
+    }
+    pub fn draw(&self, context:&Context){
+        for r in &self.roads{
+            r.draw(context);
+        }
+    }
+}
