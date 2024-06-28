6e7985d16556980a68b2fb8b7025391fd8720bac
Bridget Hartin Pasco <isaacpasco@icloud.com>
2024-06-28 15:57:56 -0700

    bruh:

---
+++ src/libnpc/target/debug/deps/libnpc-32ff7d986da90a87.d
@@ -0,0 +1,9 @@
+/Users/bridget/Cities/src/libnpc/target/debug/deps/libnpc-32ff7d986da90a87: lib.rs npc.rs npc/names.rs npc/biology.rs npc/name_gen.rs
+
+/Users/bridget/Cities/src/libnpc/target/debug/deps/libnpc-32ff7d986da90a87.d: lib.rs npc.rs npc/names.rs npc/biology.rs npc/name_gen.rs
+
+lib.rs:
+npc.rs:
+npc/names.rs:
+npc/biology.rs:
+npc/name_gen.rs:
